5a16c6bf7aa3179f50657efb6613c2c0799c28b5
Nikola Hadzic <bibilionyt@gmail.om>
2025-05-12 20:21:53 +0200

    change to localhost api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   frontendBaseUrl: 'http://localhost:4200',
-  apiUrl: 'http://WS568598.ms.uhbs.ch:9090/api',
+  apiUrl: 'http://localhost:9090/api',
   keycloak: {
     issuer: 'https://auth.nikolahadzic.net/realms/NotesApp',
     clientId: 'NotesApp',
