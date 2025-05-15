c9df95155b4f6fa7a8943871c585b305b9621f0a
Nikola Hadzic <bibilionyt@gmail.om>
2025-05-15 22:48:07 +0200

    display info button only if the user has the Role Admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,33 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIcon } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { AppRoles } from '../app.roles';
 import { HeaderComponent } from './components/header/header.component';
 import { InfoDialogComponent } from './components/info-dialog/info-dialog.component';
+import { IsInRoleDirective } from './dir/is.in.role.dir';
 import { AppAuthService } from './services/app.auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, MatIcon, CommonModule, RouterModule],
+  imports: [
+    HeaderComponent,
+    MatIcon,
+    CommonModule,
+    RouterModule,
+    IsInRoleDirective,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  readonly AppRoles = AppRoles;
   title = 'NotesApp';
 
-  constructor(private dialog: MatDialog, public authService: AppAuthService) {
+  constructor(
+    private dialog: MatDialog,
+    public authService: AppAuthService,
+  ) {
     this.authService.initAuth();
   }
 
